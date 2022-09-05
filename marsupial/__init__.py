e92195d75fc8277986c9b27204e33d5f6c330a5e
Henry Lydecker <henry.lydecker@gmail.com>
2022-09-05 10:54:42 +1000

    feature: make marsupial into a python library

---
+++ marsupial/__init__.py
@@ -0,0 +1,9 @@
+"""
+marsupial: python library for lightning fast detection and identification of animals in Australian camera trap images.
+
+"""
+
+__version__ = '1.0.0'
+__date__ = '2022-09-01'
+__author__ = 'Henry Lydecker'
+__license__ = 'MIT'
